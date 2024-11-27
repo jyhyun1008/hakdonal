6498376fc8d5f9f90560b94d795d8cb5a8f80484
HiYUNo <jayhyun1008@gmail.com>
2024-11-27 11:56:07 +0900

    typo

--- js/index.js
+++ js/index.js
@@ -9,10 +9,10 @@ async function loadContents(page) {
 }
 
 async function loadComponents(component) {
-    if (document.querySelector(page)) {
+    if (document.querySelector(component)) {
         var result = await fetch('./component/'+component+'.html', {mode: "no-cors"})
         var resultText = await result.text()
-        document.querySelectorAll(page).innerHTML = resultText
+        document.querySelectorAll(component).innerHTML = resultText
     }
 }
 
