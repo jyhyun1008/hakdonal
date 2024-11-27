785e23b9f05c153a9736db33c0bb829b8235dab8
jyhyun1008 <jayhyun1008@gmail.com>
2024-11-27 16:42:04 +0900

    동적 라우팅 테스트

---
+++ pages/store.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="container">
+        <h1>가게</h1>
+        <div class="box-content">
+            <div></div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { marked } from 'marked';
+
+</script>
---
+++ pages/story/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="container">
+        <h1>그림-모음</h1>
+        <div class="box-content">
+            <div></div>
+        </div>
+        <h1>글-모음</h1>
+        <div class="box-content">
+            <div></div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { marked } from 'marked';
+
+</script>
