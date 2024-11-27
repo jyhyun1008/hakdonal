2423bcdea11fc113a1eb0559bcdbb70323a044af
jyhyun1008 <jayhyun1008@gmail.com>
2024-11-27 16:52:37 +0900

    서브도메인 넣었으니 되돌리기

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <img src="https://github.com/jyhyun1008/hakdonal/raw/refs/heads/main/public/png/001.webp" style="width: 100%; object-fit: cover; aspect-ratio: 16 / 9;">
+        <img src="/public/png/001.webp" style="width: 100%; object-fit: cover; aspect-ratio: 16 / 9;">
         <div id="container">
             <Example />
         </div>
