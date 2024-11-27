9fd6a277527e2a66485a8636ed5452c6bbb308e4
HiYUNo <jayhyun1008@gmail.com>
2024-11-27 11:49:38 +0900

    test

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var page = location.href.split('/').pop().split('.')[0]
 
 async function loadContents(page) {
-    if (page == 'index') {
+    if (page == 'index' || !page) {
         var result = await fetch('./README.md', {mode: "no-cors"})
         var resultText = await result.text()
         document.querySelector('.box-content').innerText += resultText
@@ -9,3 +9,15 @@ async function loadContents(page) {
 }
 
 loadContents(page)
+
+async function loadComponents(component) {
+    if (document.querySelector(page)) {
+        var result = await fetch('./component/'+component+'.html', {mode: "no-cors"})
+        var resultText = await result.text()
+        document.querySelectorAll(page).innerHTML = resultText
+    }
+}
+
+loadComponents(decorator)
+loadComponents(navbar)
+loadComponents(concept)
