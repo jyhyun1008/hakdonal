ac65c57705475b1a997ede510682c99e0f955a37
jyhyun1008 <jayhyun1008@gmail.com>
2024-11-27 17:27:33 +0900

    방명록 살리기

--- components/Example.vue
+++ components/Example.vue
@@ -4,7 +4,7 @@
         <div id="guestbook">
             <div id="guestbook-form">
                 <textarea id="guestbook-textarea"></textarea>
-                <button onclick="createGuestNote(document.querySelector('#guestbook-textarea').value)">전송</button>
+                <button @click="createGuestNote">전송</button>
             </div>
             <div id="guestbook-list">
 
@@ -23,60 +23,65 @@ import { marked } from 'marked';
 
 var updateLog = await $fetch('https://raw.githubusercontent.com/jyhyun1008/hakdonal/main/md/update.md')
 
-onMounted(async ()=> {
-    const TOKEN1 = 'rXOkOzQM'
-    const TOKEN2 = 'zWzLhTcv'
-    const TOKEN3 = 'ViZGxsLt'
-    const TOKEN4 = 'Ufdweib0'
 
-    async function viewGuestNotes() {
+const TOKEN1 = 'rXOkOzQM'
+const TOKEN2 = 'zWzLhTcv'
+const TOKEN3 = 'ViZGxsLt'
+const TOKEN4 = 'Ufdweib0'
 
-        document.querySelector('#guestbook-list').innerHTML = ''
+async function viewGuestNotes() {
 
-        var endpointURL = 'https://peacht.art/api/users/notes'
-        var endpointParam = {
+    document.querySelector('#guestbook-list').innerHTML = ''
+
+    var endpointURL = 'https://peacht.art/api/users/notes'
+    var endpointParam = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            Authorization: `Bearer ${TOKEN1}${TOKEN2}${TOKEN3}${TOKEN4}`,
+        },
+        body: JSON.stringify({
+            query: 'neo_quesdon',
+            userId: 'a12xjih3zo3a08a5',
+            limit: 10
+        }),
+        credentials: 'omit',
+    }
+    var result = await fetch(endpointURL, endpointParam)
+    var resultJson = await result.json()
+
+    for await(let result of resultJson) {
+        document.querySelector('#guestbook-list').innerHTML += '<div class="guestbook-text"><span>'+result.createdAt.split('T')[0]+'</span> '+result.text+'</div>'
+    }
+}
+
+async function createGuestNote() {
+
+    var text = document.querySelector('#guestbook-textarea').value
+    if (text.length<5) {
+        alert('5글자 이상 입력해 주세요.')
+    } else {
+        fetch("https://peacht.art/api/notes/create", {
             method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-                Authorization: `Bearer ${TOKEN1}${TOKEN2}${TOKEN3}${TOKEN4}`,
-            },
             body: JSON.stringify({
-                query: 'neo_quesdon',
-                userId: 'a12xjih3zo3a08a5',
-                limit: 10
+                    text: text,
+                    visibility: 'specified',
+                    localOnly: true,
             }),
+            headers: {
+                Authorization: `Bearer ${TOKEN1}${TOKEN2}${TOKEN3}${TOKEN4}`,
+                'Content-Type': 'application/json',
+            },
             credentials: 'omit',
-        }
-        var result = await fetch(endpointURL, endpointParam)
-        var resultJson = await result.json()
-
-        for await(let result of resultJson) {
-            document.querySelector('#guestbook-list').innerHTML += '<div class="guestbook-text"><span>'+result.createdAt.split('T')[0]+'</span> '+result.text+'</div>'
-        }
+        });
+        document.querySelector('#guestbook-textarea').value = ''
+        setTimeout(() => {
+            viewGuestNotes()
+        }, 1000);
     }
+}
 
-    async function createGuestNote(text) {
-        if (text.length<5) {
-            alert('5글자 이상 입력해 주세요.')
-        } else {
-            fetch("https://peacht.art/api/notes/create", {
-                method: 'POST',
-                body: JSON.stringify({
-                        text: text,
-                        visibility: 'specified',
-                        localOnly: true,
-                }),
-                headers: {
-                    Authorization: `Bearer ${TOKEN1}${TOKEN2}${TOKEN3}${TOKEN4}`,
-                    'Content-Type': 'application/json',
-                },
-                credentials: 'omit',
-            });
-            setTimeout(() => {
-                viewGuestNotes()
-            }, 1000);
-        }
-    }
+onMounted(async ()=> {
     viewGuestNotes()
 })
 </script>
