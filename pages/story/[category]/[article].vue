df8505f0f5cbaf4d1ee3eaca48025312b30b287e
jyhyun1008 <jayhyun1008@gmail.com>
2024-11-28 17:18:03 +0900

    포스타입 글 긁어오기

--- pages/story/[category]/[article].vue
+++ pages/story/[category]/[article].vue
@@ -1,15 +1,42 @@
 <template>
     <div id="container">
-        <h1>그림-모음</h1>
-        <div class="box-content" style="display: grid; grid-template-columns: 1fr 1fr 1fr; gap: 2px;">
-            <div></div>
+        <h1>{{ content_title }}</h1>
+        <a :href="`/story/${content_category}`">{{ content_category }}</a>
+        <div class="box-content">
+            <div v-for="img of content_img"><img :src=img></div>
         </div>
-        <h1>글-모음</h1>
-        <div class="box-content" style="display: flex; flex-direction: column; gap: 2px;">
-            <div></div>
-        </div>
+        <a :href="`https://postype.com/@peacht-art/post/${route.params.article}`" target="_blank">덧글 달고 후원하러 가기</a>
     </div>
 </template>
 
 <script setup>
+
+import { onMounted } from 'vue';
+import { marked } from 'marked';
+const route = useRoute()
+
+const CORS_PROXY = "https://proxy.rongo.moe/?url=https://postype.com/@peacht-art/post/"
+let content = await $fetch(CORS_PROXY + route.params.article)
+content = content.replace(/\\/gm, '')
+
+var content_title = content.split('<h1 class="MuiTypography-root MuiTypography-body-md joy-d3qw43">')[1].split('</h1>')[0].split('-')[2]
+var content_category = content.split('<h1 class="MuiTypography-root MuiTypography-body-md joy-d3qw43">')[1].split('</h1>')[0].split('-')[1]
+
+var content_prev, content_next
+// if (content.includes('joy-6mbqgc')) {
+//     content_prev = content.split('<section class="MuiStack-root post-navigation joy-1tk24vt">')[1].split('class="MuiStack-root joy-6mbqgc" href="/@peacht-art/post/')[1].split('"')[0]
+// }
+// if (content.includes('joy-vce80x')) {
+//     content_next = content.split('<section class="MuiStack-root post-navigation joy-1tk24vt">')[1].split('class="MuiStack-root joy-vce80x" href="/@peacht-art/post/')[1].split('"')[0]
+// }
+
+let content_img = []
+
+var content_list = content.split('<div class="photoset-inner">')
+for (var j=1; j<content_list.length; j++){
+    if (content_list[j].includes('" data-caption="')){
+        content_img.push(content_list[j].split('data-caption="')[1].split('" class="photo"')[0])
+    }
+}
+
 </script>
--- pages/story/index.vue
+++ pages/story/index.vue
@@ -9,36 +9,44 @@
             <a v-for="category of categories" :href="`/story/${category}`">{{ category }}</a>
         </div>
         <div class="box-content" style="display: flex; flex-direction: column; gap: 2px;">
-            <div v-for="article of articles" class="article-items"><a :href="`/${article.category}/${article.url}`"><span>{{ article.title }}</span> <code>{{ article.category }}</code></a></div>
+            <div v-for="article of articles" class="article-items"><a :href="`/story/${article.category}/${article.url}`"><span>{{ article.title }}</span> <code>{{ article.category }}</code></a></div>
         </div>
     </div>
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
-import { marked } from 'marked';
-import Parser from 'rss-parser';
 
-const CORS_PROXY = "https://proxy.rongo.moe/?url=" //나중에 다른 서버로 바꿔줘야함
-let parser = new Parser();
-
-let feed = await parser.parseURL(CORS_PROXY + 'https://postype.com/@peacht-art/rss');
-var episodes = feed.items
 var articles = []
 var categories = []
 
-for (let j=0; j<episodes.length;j++) {
-    if (episodes[j].title.includes('학 생도들의')) {
-        articles.push({
-            title: episodes[j].title.split('-')[2],
-            category: episodes[j].title.split('-')[1],
-            url: episodes[j].link.split('post/')[1]
-        })
-        categories.push(episodes[j].title.split('-')[1])
+async function parseRss() {
+
+    const CORS_PROXY = "https://proxy.rongo.moe/?url=" //나중에 다른 서버로 바꿔줘야함
+
+    const feed = await $fetch(CORS_PROXY + 'http://postype.com/@peacht-art/rss')
+
+    var article_title = feed.split('<item><title><![CDATA[')
+    var article_pubdate = feed.split('<pubDate>')
+    var article_link = feed.split('isPermaLink="true">')
+
+    for (let i=0; i< article_title.length-1; i++) {
+        if (article_title[i+1].includes('학 생도들의')) {
+            articles.push({
+                title:article_title[i+1].split(']]></title>')[0].split('-')[2],
+                category: article_title[i+1].split(']]></title>')[0].split('-')[1],
+                url: article_link[i+1].split('</guid>')[0].split('post/')[1],
+                pubDate: article_pubdate[i+1].split('</pubDate>')[0]
+            })
+            categories.push(article_title[i+1].split(']]></title>')[0].split('-')[1])
+        }
     }
+
+    var categorieset = new Set(categories);
+    categories = [...categorieset];
+
 }
 
-var categorieset = new Set(categories);
-categories = [...categorieset];
+await parseRss()
+
 
 </script>
